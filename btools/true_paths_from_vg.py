30dc85ca9a90b73eb0fc527231199727c6696b33
Aaron Darling <aaron.darling@uts.edu.au>
2018-01-31 19:23:58 +1100

    script to build a node label table for Seb's Bandage color program

---
+++ btools/true_paths_from_vg.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import json
+import sys
+
+# set up a reverse complement table
+fwd_chars = "ACGTacgt"
+rev_chars = "TGCAtgca"
+rctab = str.maketrans(fwd_chars,rev_chars)
+
+# TODO: this approach of slurping in the whole graph might be too memory hungry
+print("Reading assembly graph JSON from "+sys.argv[1])
+asm_file = open(sys.argv[1])
+asm_json = json.load(asm_file)
+seq_id_map = {}
+for seq in asm_json["node"]:
+    seq_id_map[seq["id"]]=seq["sequence"]
+
+print("Reading mapped reads from "+sys.argv[2])
+json_file = open(sys.argv[2])
+
+seq_id_bins = {}
+bin_id=-1
+for line in json_file:
+    bin_id += 1
+    gam = json.loads(line)
+    for mapping in gam["path"]["mapping"]:
+        node_id = mapping["position"]["node_id"]
+        if not node_id in seq_id_bins: seq_id_bins[node_id]={}
+        seq_id_bins[node_id][bin_id]=1
+
+for node in seq_id_bins:
+    node_bins = str(node)
+    for b in seq_id_bins[node]:
+        node_bins += "\t"+str(b)
+    print(node_bins)
