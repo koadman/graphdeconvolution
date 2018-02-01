a094ce5ac00380b3856e5a57ecdd479b362deac0
Aaron Darling <aaron.darling@uts.edu.au>
2018-02-01 18:22:05 +1100

    bugfix for erroneous assumptions on the content of stan output matrix columns

--- stan_models/summarize.py
+++ stan_models/summarize.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from __future__ import division
 import sys
+import numpy as np
 if len(sys.argv) != 5:
     print "Usage: summarize.py <stan data> <stan posterior> <FastA output> <relevance threshold>"
     sys.exit(-1)
@@ -8,7 +9,6 @@ datafile = open(sys.argv[1])
 postfile = open(sys.argv[2])
 outfile = open(sys.argv[3], 'w')
 relevance_threshold = float(sys.argv[4])
-
 
 
 genomes = 0
@@ -23,32 +23,19 @@ for line in datafile:
 
 pprobs = {}
 relevance = {}
-samples = 0.0
-for line in postfile:
-    if line[0] == '#':
-        continue
-    if line[0] != '0':
-        continue  # lp__ is always 0
-    l = line.rstrip("\n").split(',')
-    samples += 1.0
-    # this is awful and hackish. parsing out node probabilities positionally instead of by looking at column headers
-    i = len(l) - genomes*unitigs - 2*genomes
+
+popofile = np.genfromtxt(sys.argv[2],comments='#',skip_header=28,delimiter=',',names=True,deletechars="""~!@#$%^&*()=+~\|]}[{';: /?>,<""")
+
+for u in range(unitigs):    
     for g in range(genomes):
         if not g in pprobs: pprobs[g] = {}
-        for v in range(unitigs):
-            if not v in pprobs[g]: pprobs[g][v] = 0.0
-            pprobs[g][v] += float(l[i])
-            i += 1
-    # parse relevance in the same manner
-    i = len(l) - 2*genomes
-    for g in range(genomes):
-        if not g in relevance: relevance[g] = 0.0
-        relevance[g] += float(l[i])
-        i+=1
+        genotxt=popofile['geno.'+str(u+1)+'.'+str(g+1)]        
+        pprobs[g][u]=np.sum(genotxt)/len(genotxt)
 
-for g in range(genomes):
-    relevance[g] /= samples
-    print "relevance " + str(relevance[g])
+for r in range(genomes):
+    rtxt=popofile['relevance.'+str(r+1)]
+    relevance[r]=np.sum(rtxt)/len(rtxt)
+    print("relevance "+str(relevance[r]))
 
 # write posterior mean
 relevant=0
@@ -59,7 +46,7 @@ for v in range(unitigs):
             continue # skip genome if not relevant
         relevant+=1
         outfile.write(sep)
-        outfile.write( str(pprobs[g][v] / samples) )
+        outfile.write( str(pprobs[g][v]) )
         sep = "\t"
     outfile.write("\n")
 
